e1ac601e7db68d20d6beec21203eca8ad33b0657
jakev220 <jrvillasenor@ucsd.edu>
2023-04-26 16:04:54 -0700

    biomed-sizes.txt

--- count-txts.sh
+++ count-txts.sh
@@ -1,3 +1,11 @@
 find technical > find-results.txt
 grep ".txt" find-results.txt > grep-results.txt
 wc grep-results.txt
+
+find technical/biomed > find-results2.txt
+grep ".txt" find-results2.txt > grep-results2.txt
+wc grep-results2.txt
+
+find technical/government > find-results3.txt
+grep ".txt" find-results3.txt > grep-results3.txt
+wc grep-results3.txt
